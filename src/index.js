8791834ddf294738762c3fbb51b3f23a0cfa11a4
Yahor Simak <simak_yahor@mail.ru>
2018-01-10 22:06:29 +0300

    add implementation of positive boolset task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function getPositiveKeysOfBoolset(boolset) {
-  // your solution here
+  let output = [];
+  for (let i = 1; i < boolset.length; i += 2) {
+    boolset[i] === true && output.push(boolset[i-1]);
+  }
+  return output;
 }
